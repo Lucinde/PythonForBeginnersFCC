0e5683c71d2d1e059611ea8dc6cba187ea4c429d
Lucinde <lucinde@lucinde.nl>
2023-09-25 16:57:52 +0200

    Lesson 5- User Input

---
+++ LESSON05/rps.py
@@ -0,0 +1,44 @@
+import random
+import sys
+from enum import Enum
+
+
+class RPS(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+# print(RPS(2))
+# print(RPS.ROCK)
+# print(RPS["ROCK"])
+# print(RPS.ROCK.value)
+# sys.exit()
+
+print("")
+playerchoice = input("Enter ...\n1 for rock,\n2 for paper, or \n3 for scissors:\n\n")
+
+player = int(playerchoice)
+
+if player < 1 | player > 3:
+    sys.exit("You must enter 1, 2 or 3.")
+
+computerchoice = random.choice("123")
+
+computer = int(computerchoice)
+
+print("")
+print("You chose " + str(RPS(player)).replace("RPS.", "") + ".")
+print("Python chose " + str(RPS(computer)).replace("RPS.", "") + ".")
+print("")
+
+if player == 1 and computer == 3:
+    print("You win! 🎉")
+elif player == 2 and computer == 1:
+    print("You win! 🎉")
+elif player == 3 and computer == 2:
+    print("You win! 🎉")
+elif player == computer:
+    print("😲 Tie game!")
+else:
+    print("🐍 Python wins!")
