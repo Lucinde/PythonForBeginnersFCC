496314616f6ff980a6d5e5b1123ebf4035402b8d
Lucinde <lucinde@lucinde.nl>
2023-10-27 11:47:23 +0200

    Lesson 19 - errors

--- LESSON19/exceptions.py
+++ LESSON19/exceptions.py
@@ -8,8 +8,8 @@ try:
     raise JustNotCoolError("This just isn't cool, man.")
     # raise Exception("I'm a custom exception!")
     # print(x / 1)
-    if not type(x) is str:
-        raise TypeError("Only strings are allowed.")
+    # if not type(x) is str:
+    #     raise TypeError("Only strings are allowed.")
 except NameError:
     print("NameError means something is probably undefined.")
 except ZeroDivisionError:
